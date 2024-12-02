372c6c849db4be0b08ad155de29bb9f8abb4d184
BigHeadTiger <119154750+BigHeadTiger@users.noreply.github.com>
2024-12-02 15:42:27 +0800

    Update talkatone.js

--- talkatone.js
+++ talkatone.js
@@ -6,7 +6,7 @@ body = body.replace(/<div class=".*?ad-banner-top.*?">.*?<\/div>/g, '');
 
 // 去除中部的 "Premium SMS" 提示广告
 body = body.replace(/<div class=".*?premium-sms-ad.*?">.*?<\/div>/g, '');
-body = body.replace(/<div class=".*?Ads.*?">.*?<\/div>/g, '');
+body = body.replace(/<div class=".*?Ads-ad.*?">.*?<\/div>/g, '');
 
 // 去除底部广告横幅
 body = body.replace(/<div class=".*?ad-banner-bottom.*?">.*?<\/div>/g, '');
