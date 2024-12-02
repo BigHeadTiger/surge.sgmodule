504fd5069b471bbea91c77d8a0fa8cd2eee66088
BigHeadTiger <119154750+BigHeadTiger@users.noreply.github.com>
2024-12-02 20:56:27 +0800

    Update talkatone.js

--- talkatone.js
+++ talkatone.js
@@ -1,14 +1,19 @@
-// talkatone_ad_block.js
+// 获取响应体内容
 let body = $response.body;
 
-// 去除顶部广告横幅
-body = body.replace(/<div class=".*?ad-banner-top.*?">.*?<\/div>/g, '');
+// 移除顶部广告
+body = body.replace(/<div id="top-banner".*?<\/div>/g, '');
 
-// 去除中部的 "Premium SMS" 提示广告
-body = body.replace(/<div class=".*?premium-sms-ad.*?">.*?<\/div>/g, '');
-// body = body.replace(/<div class=".*?ads-section.*?">.*?<\/div>/g, '');
+// 移除中部广告
+body = body.replace(/<div id="middle-banner".*?<\/div>/g, '');
 
-// 去除底部广告横幅
-body = body.replace(/<div class=".*?ad-banner-bottom.*?">.*?<\/div>/g, '');
+// 移除底部广告
+body = body.replace(/<div id="bottom-banner".*?<\/div>/g, '');
 
+// 优化布局
+body = body.replace(/padding-top:.*?;/g, 'padding-top: 0px;'); // 删除顶部空白
+body = body.replace(/padding-bottom:.*?;/g, 'padding-bottom: 0px;'); // 删除底部空白
+body = body.replace(/<div class="ad-container".*?<\/div>/g, ''); // 针对通用广告容器
+
+// 返回修改后的结果
 $done({ body });
